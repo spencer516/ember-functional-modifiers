5bf4b6d7d71181e15080fd6647e6a51e24cfe208
Spencer Price <spencer516@gmail.com>
2019-03-02 07:00:02 -0500

    setup initial configuration

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  name: require('./package').name
+  name: require("./package").name,
+  options: {
+    babel: {
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+        ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: false }]
+      ]
+    }
+  }
 };
