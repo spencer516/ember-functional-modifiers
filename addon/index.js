bd59dbdeaf4dc6b2daa731db3306fa57fa6296dc
Spencer Price <spencer516@gmail.com>
2019-03-02 11:53:31 -0500

    do not use intermediate classes (v2)

--- addon/index.js
+++ addon/index.js
@@ -1,9 +1,47 @@
-// TODO: I expected this to be imported were it exported in modifier-managers
-import './-private/funtional-manager';
-import FunctionalModifier from './modifiers/functional-modifier';
+import Ember from 'ember';
 
-export default function makeFunctionaModifier(fn) {
-  return class CustomModifier extends FunctionalModifier {
-    modifierFunction = fn;
-  };
+const MODIFIER_STATE = new WeakMap();
+
+function teardown(modifier) {
+  if (!MODIFIER_STATE.has(modifier)) {
+    return;
+  }
+
+  const { teardown, element } = MODIFIER_STATE.get(modifier);
+
+  if (teardown && typeof teardown === 'function') {
+    teardown();
+  }
+
+  return element;
+}
+
+function setup(modifier, element, args) {
+  const { positional, named } = args;
+  const teardown = modifier(element, positional, named);
+
+  MODIFIER_STATE.set(modifier, { element, teardown });
+}
+
+export default function makeFunctionalModifier(fn) {
+  return Ember._setModifierManager(
+    () => ({
+      createModifier(factory) {
+        // https://github.com/rwjblue/ember-modifier-manager-polyfill/issues/6
+        const fn = factory.class ? factory.class : factory;
+        return (...args) => fn(...args);
+      },
+      installModifier(modifier, element, args) {
+        setup(modifier, element, args);
+      },
+      updateModifier(modifier, args) {
+        const element = teardown(modifier);
+        setup(modifier, element, args);
+      },
+      destroyModifier(modifier) {
+        teardown(modifier);
+      }
+    }),
+    fn
+  );
 }
