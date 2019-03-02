b2915ba8c3421cd57e0ebb5bfd4d1945eb6b581c
Spencer Price <spencer516@gmail.com>
2019-03-02 07:08:00 -0500

    add polyfill to ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,18 +7,21 @@ module.exports = function() {
     getChannelURL('release'),
     getChannelURL('beta'),
     getChannelURL('canary')
-  ]).then((urls) => {
+  ]).then(urls => {
     return {
       scenarios: [
         {
           name: 'ember-lts-2.18',
           env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': true
+            })
           },
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
+              'ember-source': '~2.18.0',
+              'ember-modifier-manager-polyfill': '^1.0.2'
             }
           }
         },
@@ -26,7 +29,8 @@ module.exports = function() {
           name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0'
+              'ember-source': '~3.4.0',
+              'ember-modifier-manager-polyfill': '^1.0.2'
             }
           }
         },
