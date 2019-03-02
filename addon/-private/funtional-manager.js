8a13ddcb75ec93488e6553abf250107f12222443
Spencer Price <spencer516@gmail.com>
2019-03-02 08:06:10 -0500

    handle polyfill

--- addon/-private/funtional-manager.js
+++ addon/-private/funtional-manager.js
@@ -4,7 +4,12 @@ import FunctionalModifier from '../modifiers/functional-modifier';
 export default Ember._setModifierManager(
   () => ({
     createModifier(factory) {
-      return new factory.class();
+      // https://github.com/rwjblue/ember-modifier-manager-polyfill/issues/6
+      if (factory.class) {
+        return new factory.class();
+      } else {
+        return new factory();
+      }
     },
     installModifier(instance, element, args) {
       instance.element = element;
